4c391e3cb7694609800768dc9374cddd6a22facb
carlosrrdev <rr.carlos.dev@gmail.com>
2024-02-10 18:27:32 -0600

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+	import '../global.css';
+</script>
+
+<div class="container">
+	<div>
+		<slot />
+	</div>
+</div>
+
+<style>
+    .container {
+        width: 100%;
+        min-height: 100dvh;
+        overflow-x: hidden;
+        position: relative;
+        background-image:
+				url("/noise.svg"),
+                radial-gradient(at 65% 0%, rgba(255, 193, 122, .4) 0px, transparent 65%),
+                radial-gradient(at 50% 100%, rgba(31, 221, 225, .1) 0px, transparent 95%),
+                radial-gradient(at 80% 50%, rgba(255, 133, 173, .1) 0px, transparent 50%),
+                radial-gradient(at 0% 100%, rgba(255, 181, 138, 0) 0px, transparent 50%),
+                radial-gradient(at 80% 100%, rgba(107, 102, 255, 0) 0px, transparent 50%),
+                radial-gradient(at 0% 0%, rgba(255, 133, 167, .25) 0px, transparent 50%);
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<div class="container">
+	<h1 class="title">crr.</h1>
+	<nav>
+		<ul class="links">
+			<li>
+				<a href="/projects">Projects</a>
+			</li>
+			<li class="link-center">
+				<a href="/blog">Blog</a>
+			</li>
+			<li>
+				<a href="/resume">Resume</a>
+			</li>
+		</ul>
+	</nav>
+</div>
+
+<style>
+		.title {
+				font-size: clamp(1rem, 20vh, 8rem);
+				font-weight: 700;
+		}
+	.container {
+			display: flex;
+			flex-direction: column;
+      min-height: 100dvh;
+			justify-content: center;
+			align-items: center;
+	}
+	.links {
+			display: flex;
+			column-gap: 1rem;
+	}
+	.link-center {
+			padding: 0 1rem;
+			border-left: 2px solid var(--font-clr);
+			border-right: 2px solid var(--font-clr);
+	}
+</style>
